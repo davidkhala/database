01911f881d43f0825e651cb43b080a158df8982c
David Liu <david-khala@hotmail.com>
2023-01-12 19:39:14 +0800

    Update xpand.sh

--- maria/xpand.sh
+++ maria/xpand.sh
@@ -1,5 +1,5 @@
 run() {
-  docker run -d--name mariadb-xpand --publish '3306:3306/tcp' mariadb/xpand-single:latest --mini
+  docker run -d --name mariadb-xpand --publish '3306:3306/tcp' mariadb/xpand-single:latest --mini
 }
 exec(){
   docker exec --interactive --tty mariadb-xpand mariadb --user=xpand --password
