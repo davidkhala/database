8728d4f82c05af4416a75504bb36bb809c58a78b
David Liu <david-khala@hotmail.com>
2023-01-13 16:29:12 +0800

    Update install.sh

--- trino/install.sh
+++ trino/install.sh
@@ -1,6 +1,6 @@
 trino-CLI(){
   java -version
-  wget https://repo1.maven.org/maven2/io/trino/trino-cli/405/trino-cli-405-executable.jar trino
+  wget https://repo1.maven.org/maven2/io/trino/trino-cli/405/trino-cli-405-executable.jar -O trino
   chmod +x ./trino
   ./trino --version
 
