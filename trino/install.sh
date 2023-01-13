f3f0db40a5c03973410b4dcb2db83a502286082e
David Liu <david.yx.liu@oracle.com>
2023-01-13 16:53:21 +0800

    Update install.sh

--- trino/install.sh
+++ trino/install.sh
@@ -1,10 +1,14 @@
 set -e
-trino-CLI(){
+trino-CLI() {
   java -version
   wget https://repo1.maven.org/maven2/io/trino/trino-cli/405/trino-cli-405-executable.jar -O trino
   chmod +x ./trino
   ./trino --version
 
+}
+login() {
+  
+  ./trino --server http://${domain:-localhost}:8080
 }
 
 $@
