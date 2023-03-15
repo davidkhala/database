5fc286292a9207d4e2fd32e2bedd92b5fed90751
David Liu <david-khala@hotmail.com>
2023-03-15 20:15:13 +0800

    Update teradata.sh

--- teradata/express/teradata.sh
+++ teradata/express/teradata.sh
@@ -20,13 +20,14 @@ start-attach() {
     vboxmanage createvm --name "$VM_NAME" --register --ostype openSUSE_64
     vboxmanage modifyvm "$VM_NAME" --ioapic on --memory 6000 --vram 128 --nic1 nat --cpus 4
     vboxmanage storagectl "$VM_NAME" --name "SATA Controller" --add sata --controller IntelAhci
-    vboxmanage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$(find $VM_IMAGE_DIR -name '*disk1*')"
-    vboxmanage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 1 --device 0 --type hdd --medium "$(find $VM_IMAGE_DIR -name '*disk2*')"
-    vboxmanage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 2 --device 0 --type hdd --medium "$(find $VM_IMAGE_DIR -name '*disk3*')"
+    vboxmanage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  "$(find $VM_IMAGE_DIR -name '*disk1*')"
+    vboxmanage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 1 --device 0 --type hdd --medium  "$(find $VM_IMAGE_DIR -name '*disk2*')"
+    vboxmanage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 2 --device 0 --type hdd --medium  "$(find $VM_IMAGE_DIR -name '*disk3*')"
     vboxmanage modifyvm "$VM_NAME" --natpf1 "tdssh,tcp,,4422,,22"
     vboxmanage modifyvm "$VM_NAME" --natpf1 "tddb,tcp,,1025,,1025"
     vboxmanage startvm "$VM_NAME" --type headless
     vboxmanage controlvm "$VM_NAME" keyboardputscancode 1c 1c
+
 }
 login-vm() {
     ssh -p 4422 root@localhost
