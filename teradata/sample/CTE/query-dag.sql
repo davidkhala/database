f5e53ab31d4a4944b93843ecd01b1be6d248c663
David Liu <david-khala@hotmail.com>
2023-05-04 10:18:59 +0800

    Update query-dag.sql

--- teradata/sample/CTE/query-dag.sql
+++ teradata/sample/CTE/query-dag.sql
@@ -1,14 +1,16 @@
-WITH multiple_orders AS (
-   SELECT customer_id, COUNT(*) AS order_count
-   FROM orders
-   GROUP BY customer_id
-   HAVING COUNT(*) > 1
-),
-multiple_order_totals AS (
+-- support subsequently definition (defer define)
+
+WITH multiple_order_totals AS (
    SELECT customer_id, SUM(total_cost) AS total_spend
    FROM orders
    WHERE customer_id IN (SELECT customer_id FROM multiple_orders) 
    GROUP BY customer_id
+),
+multiple_orders AS (
+   SELECT customer_id, COUNT(*) AS order_count
+   FROM orders
+   GROUP BY customer_id
+   HAVING COUNT(*) > 1
 )
 SELECT * FROM multiple_order_totals
 ORDER BY total_spend DESC;
