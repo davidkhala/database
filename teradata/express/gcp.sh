26c3b9bc70f15b01f39ebab72b84ae81ae1a7dad
David Liu <david-khala@hotmail.com>
2023-06-05 10:57:59 +0800

    add vm backup

--- teradata/express/gcp.sh
+++ teradata/express/gcp.sh
@@ -1,10 +1,15 @@
+vm_name=${name:-teradata-vantage-express}
+zone=${zone:-asia-east2-c}
 create-vm(){
-  gcloud compute instances create teradata-vantage-express --zone=asia-east2-c --machine-type=n2-custom-4-8192 --enable-nested-virtualization \
-  --create-disk=boot=yes,device-name=ve-disk,image-project=ubuntu-os-cloud,image-family=ubuntu-2004-lts,size=70,type=pd-balanced 
+  gcloud compute instances create ${vm_name} --zone=${zone} --machine-type=n2-custom-4-8192 --enable-nested-virtualization \
+    --create-disk=boot=yes,device-name=ve-disk,image-project=ubuntu-os-cloud,image-family=ubuntu-2004-lts,size=70,type=pd-standard 
 
 }
+backup-vm() {
+  gcloud compute machine-images create $1 --source-instance=${vm_name}
+}
 ssh-vm(){
-  gcloud compute ssh teradata-vantage-express --zone=asia-east2-c
+  gcloud compute ssh ${vm_name} --zone=${zone}
 }
 
 $@
