2d51170b31fcbb1c7d805a413288651e3d50c17e
David Liu <david.yx.liu@oracle.com>
2023-06-06 10:35:10 +0800

    [WIP] test pipeline

--- teradata/express/gcp.sh
+++ teradata/express/gcp.sh
@@ -1,20 +1,21 @@
 vm_name=${name:-teradata-vantage-express}
 zone=${zone:-asia-east2-c}
-create-vm(){
+create-vm() {
   gcloud compute instances create ${vm_name} --zone=${zone} --machine-type=n2-custom-4-8192 --enable-nested-virtualization \
-    --create-disk=boot=yes,device-name=ve-disk,image-project=ubuntu-os-cloud,image-family=ubuntu-2004-lts,size=70,type=pd-standard 
+    --create-disk=boot=yes,device-name=ve-disk,image-project=ubuntu-os-cloud,image-family=ubuntu-2004-lts,size=70,type=pd-standard
 
 }
 backup-vm() {
   gcloud compute machine-images create $1 --source-instance=${vm_name}
 }
-ssh-vm(){
+ssh-vm() {
   gcloud compute ssh ${vm_name} --zone=${zone}
+}
+setup-vm() {
+  gcloud compute ssh ${vm_name} --zone=${zone} --command="wget https://raw.githubusercontent.com/davidkhala/databases/main/teradata/express/teradata.sh; ./teradata.sh setup"
+}
+delete-vm() {
+  gcloud compute instances delete ${vm_name} --zone=${zone}
 }
 
 $@
-
-
-
-
-  
