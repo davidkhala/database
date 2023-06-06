fb48664b8165410e3eae066a70d8df2abeda95ed
David Liu <david.yx.liu@oracle.com>
2023-06-06 11:35:59 +0800

    try wait for ssh

--- teradata/express/gcp.sh
+++ teradata/express/gcp.sh
@@ -3,7 +3,7 @@ export zone=${zone:-asia-east2-c}
 create-vm() {
   gcloud compute instances create ${vm_name} --zone=${zone} --machine-type=n2-custom-4-8192 --enable-nested-virtualization \
     --create-disk=boot=yes,device-name=ve-disk,image-project=ubuntu-os-cloud,image-family=ubuntu-2004-lts,size=70,type=pd-standard
-
+  curl https://raw.githubusercontent.com/davidkhala/gcp-collections/main/compute/cli/gcloud.sh | bash -s wait4ssh ${vm_name}
 }
 backup-vm() {
   gcloud compute machine-images create $1 --source-instance=${vm_name}
