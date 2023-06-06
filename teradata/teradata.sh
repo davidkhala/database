237ceb99c0ef6f8b2481d5eea2dd300d92473b0f
David Liu <david.yx.liu@oracle.com>
2023-06-06 12:40:25 +0800

    [wip] wait-until

--- teradata/teradata.sh
+++ teradata/teradata.sh
@@ -1,5 +1,14 @@
 db-health() {
-  pdestate -a # validate the db is up
+  # validate the db is up
+  if ! pdestate -a | grep "PDE state is RUN/STARTED."; then
+    exit 1
+  fi
+  if ! pdestate -a | grep "DBS state is 5: Logons are enabled - The system is quiescent"; then
+    exit 1
+  fi
 
+}
+wait-until-health() {
+  curl -sSL https://raw.githubusercontent.com/davidkhala/linux-utils/main/wait-until.sh | sudo bash -s db-health
 }
 $@
