5eb2bb3fd367f03365d1dc6ea1d8092cb677a6d2
David Liu <david.yx.liu@oracle.com>
2023-06-07 15:35:52 +0800

    instead of exit shell. use return to continue

--- teradata/teradata.sh
+++ teradata/teradata.sh
@@ -2,17 +2,17 @@ set -e
 db-health() {
   # validate the db is up
   if ! pdestate -a | grep "PDE state is RUN/STARTED."; then
-    exit 1
+    return 1
   fi
   if ! pdestate -a | grep "DBS state is 5: Logons are enabled - The system is quiescent"; then
-    exit 1
+    return 1
   fi
 }
 wait-until-health() {
   counter=0
   while true; do
     if db-health; then
-      break;
+      break
     else
       ((counter++))
       sleep 1
