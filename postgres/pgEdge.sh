fa2fc9bd619728212c18935db49222124450b6a0
David Liu <david-khala@hotmail.com>
2023-10-11 11:39:41 +0800

    Update pgEdge.sh

--- postgres/pgEdge.sh
+++ postgres/pgEdge.sh
@@ -1,4 +1,5 @@
 install(){
+  echo "OS must be Linux"
   python3 -c "$(curl -fsSL https://pgedge-download.s3.amazonaws.com/REPO/install.py)"
   ./pgedge/nodectl install pgedge # -U user -P passwd -d mydb
 }
