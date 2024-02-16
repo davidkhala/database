0a6192f5cea14f07012a730d0871a0b70c751fa7
David Liu <david-khala@hotmail.com>
2024-02-16 21:18:27 +0800

    Update gcp.sh

--- teradata/express/gcp.sh
+++ teradata/express/gcp.sh
@@ -17,7 +17,7 @@ ssh-vm() {
 setup-vm() {
   gcloud compute ssh ${vm_name} --zone=${zone} --command="wget https://raw.githubusercontent.com/davidkhala/databases/main/teradata/express/teradata.sh; chmod +x ./teradata.sh;"
   gcloud compute ssh ${vm_name} --zone=${zone} --command="./teradata.sh setup"
-  gcloud compute ssh ${vm_name} --zone=${zone} --command="./teradata.sh setup-vbox"
+  gcloud compute ssh ${vm_name} --zone=${zone} --command="./teradata.sh wait-until-ready"
 
   curl https://raw.githubusercontent.com/davidkhala/gcp-collections/main/compute/cli/instance.sh | bash -s get-ip ${vm_name}
 
