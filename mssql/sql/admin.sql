8f7ececf9e859e15ef8506e38b66a0eb48991db8
David Liu <david-khala@hotmail.com>
2024-05-07 02:39:35 -0700

    Update admin.sql

--- mssql/sql/admin.sql
+++ mssql/sql/admin.sql
@@ -1,3 +1,9 @@
 -- All tables across all databases:
 SELECT *
 FROM information_schema.tables
+
+-- enable SA
+ALTER LOGIN [sa] ENABLE
+
+-- change SA password
+ALTER LOGIN [sa] WITH PASSWORD='newpassword', CHECK_POLICY=OFF
