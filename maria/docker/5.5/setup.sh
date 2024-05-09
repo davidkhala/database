c72988f40395db6899b2e83d71effd43ac0bd07f
David Liu <david-khala@hotmail.com>
2024-05-09 14:54:06 +0800

    [WIP] old mariadb install

---
+++ maria/docker/5.5/setup.sh
@@ -0,0 +1,6 @@
+/usr/local/mysql/bin/mysqld_safe --user=mysql
+# while ! /usr/local/mysql/bin/mysqladmin -u root password ${MARIADB_ROOT_PASSWORD}; do
+#     sleep 1
+# done
+
+# /usr/local/mysql/bin/mysqladmin -u root password ${MARIADB_ROOT_PASSWORD}
