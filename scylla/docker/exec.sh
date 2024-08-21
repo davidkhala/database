b0f94d0f32de5b39cb724a1215b316bebc0a0865
David Liu <david-khala@hotmail.com>
2024-08-21 13:58:54 +0800

    Update exec.sh

--- scylla/docker/exec.sh
+++ scylla/docker/exec.sh
@@ -6,10 +6,15 @@ status() {
   docker exec -it $containerName nodetool status  
 }
 cql(){
+  # Interactive
   docker exec -it $containerName cqlsh $@
 }
-
-
+restart() {
+  docker exec -it $containerName supervisorctl restart scylla
+}
+dirty2(){
+  docker run -d scylladb/scylla --seeds="$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' $containerName)"
+}
 
 
 $@
