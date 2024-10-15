307e64d22d12499aa10180aa642539a5d019d121
David Liu <david-khala@hotmail.com>
2024-10-15 06:48:59 +0000

    format

---
+++ postgres/vendor/edb/as.sh
@@ -0,0 +1,1 @@
+db_bin=$(echo /usr/edb/as*/bin)
--- postgres/vendor/edb/efm.sh
+++ postgres/vendor/edb/efm.sh
@@ -1,18 +1,17 @@
 set -e
-efm(){
+efm() {
    /usr/edb/efm-*/bin/efm $@
 }
-version(){
+version() {
    efm --version
 }
 
-configure(){
+configure() {
    cd /etc/edb/efm-*/
    sudo cp efm.properties.in efm.properties
    # After copying the template file, change the owner of the file to efm:
    sudo chown efm:efm efm.properties
    cd -
 }
-
 
 $@
