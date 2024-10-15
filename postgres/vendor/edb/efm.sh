a5b7b8ff2a0bcbbead9821a947f879b59eabc720
David Liu <david-khala@hotmail.com>
2024-10-15 14:30:13 +0800

    Update efm.sh

--- postgres/vendor/edb/efm.sh
+++ postgres/vendor/edb/efm.sh
@@ -1,8 +1,17 @@
+set -e
 efm(){
    /usr/edb/efm-*/bin/efm $@
 }
 version(){
    efm --version
+}
+
+configure(){
+   cd /etc/edb/efm-*/
+   sudo cp efm.properties.in efm.properties
+   # After copying the template file, change the owner of the file to efm:
+   sudo chown efm:efm efm.properties
+   cd -
 }
 
 
