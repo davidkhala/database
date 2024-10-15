2ee61ff5d9a1bc3276cc4e8815dedb2b919d9d29
David Liu <david-khala@hotmail.com>
2024-10-15 15:42:41 +0800

    Update as.sh

--- postgres/vendor/edb/as.sh
+++ postgres/vendor/edb/as.sh
@@ -1,1 +1,12 @@
+set -x
 db_bin=$(echo /usr/edb/as*/bin)
+
+setup(){
+  # post install
+  sudo PGSETUP_INITDB_OPTIONS="-E UTF-8" /usr/edb/as*/bin/edb-as-11-setup initdb
+}
+connect(){
+  psql -d edb -U enterprisedb
+}
+
+$@
