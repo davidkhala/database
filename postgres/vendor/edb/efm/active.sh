92525ee22b16647d0d8bc754495e7dd71e02a1d0
Cloud User <ec2-user@ip-172-31-6-7.ap-east-1.compute.internal>
2024-10-24 09:17:05 +0000

    backup

--- postgres/vendor/edb/efm/active.sh
+++ postgres/vendor/edb/efm/active.sh
@@ -1,7 +1,8 @@
 set -e
 cluster_name=efm
 allow-standby() {
-    sudo -u efm /usr/edb/efm-*/bin/efm allow-node $cluster_name $1
+    local standbyIP=$1
+    sudo -u efm /usr/edb/efm-*/bin/efm allow-node $cluster_name $standbyIP
     curl https://raw.githubusercontent.com/davidkhala/database/refs/heads/main/postgres/vendor/edb/efm/efm.sh | bash -s status
 }
 $@
