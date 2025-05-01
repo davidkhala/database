499b81b5c19ca6ba21d347c68328acdcf9c21367
David Liu <david-khala@hotmail.com>
2025-05-01 22:12:05 +0800

    add tsstate

--- couchbase/Capella/setup.tf
+++ couchbase/Capella/setup.tf
@@ -5,9 +5,18 @@ terraform {
     }
   }
 }
+variable "CAPELLA_AUTH_TOKEN" {
+  description = "API Secret for Couchbase Capella"
+  type        = string
+  sensitive   = true
+}
 
 # Configure the Couchbase Capella Provider 
 provider "couchbase-capella" {
-  authentication_token = var.couchbasecapella_auth_token
+  authentication_token = var.CAPELLA_AUTH_TOKEN
 }
-# Create the resources
+
+resource "couchbase-capella_project" "project" {
+  organization_id = "a1ee45af-4265-4aa3-97cb-c10678291f4a"
+  name            = "free"
+}
